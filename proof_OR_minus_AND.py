dd2b842f3d95646d5eeec5204b28e88fee53aaa0
mshachnai <m.shachnai@rutgers.edu>
2021-01-31 18:14:02 -0500

    z3 proof for the <AND, OR-AND> formulation. Works reasonably fast up until 16 bits

--- proof_OR_minus_AND.py
+++ proof_OR_minus_AND.py
@@ -5,26 +5,31 @@
 import z3
 #from z3 import * considered bad practice but defines namespace z3
 
-# List of Variables
-a, ta_val, ta_mask, b, tb_val, tb_mask, c, tc_val, tc_mask, out1, out2 = z3.BitVecs('a ta_val ta_mask b tb_val tb_mask c tc_val tc_mask out1 out2', 8)
+#change the amount of bits in the tnum to see that indeed the proof holds for all 
+#values (within reason)
+bits = 16
 
-X = [ z3.BitVec('x%s' % i, 8) for i in range(4) ]
-print(X)
+# List of Variables
+a, ta_val, ta_mask, b, tb_val, tb_mask, c, tc_val, tc_mask, out1, out2 = z3.BitVecs('a ta_val ta_mask b tb_val tb_mask c tc_val tc_mask out1 out2', bits)
+
+
+#create set that contains at most 2**n elements based on number of bits in tnum
+X = [ z3.BitVec('x%s' % i, bits) for i in range(2**bits) ]
+#print(X)
 
 f_AND = X[0]
 f_OR = X[0]
 AND_max = X[0]
  
-#s = z3.Solver()
-#for i in range (4):
 
 #this is for testing some syntax
-#And_constraint = [X[i] <= X[i+1] for i in range(3)]
+range_constraint = z3.And([X[i] <= X[i+1] for i in range(2**bits-1)])
+tnum_def_constraint = z3.And([X[i] & ~ta_mask == ta_val for i in range(2**bits)])
 #print(And_constraint)
 
 #constraints  
-And_constraint = z3.And(X[0] <= X[1], X[1] <= X[2], X[2] <= X[3])
-not_eq_constraint = z3.And(X[0] != X[1], X[1] != X[2], X[2] != X[3])
+#And_constraint = z3.And(X[0] <= X[1], X[1] <= X[2], X[2] <= X[3])
+#not_eq_constraint = z3.And(X[0] != X[1], X[1] != X[2], X[2] != X[3])
 #min_max = X[0] & X[3]
 #not_empty = z3.And(X[0] >= 0, X[1] >= 0, X[2] >= 0, X[3] >= 0)
 
@@ -46,9 +51,13 @@ xor_property = ta_mask ^ ta_val == 0
 #print (f_AND)
 #print (min_max)
 #z3.solve(And_constraint, p3,p4,p5,p6,  f_AND == ta_val, f_OR-f_AND == ta_mask)
-formula = z3.ForAll([X[0], X[1], X[2], X[3],ta_mask, ta_val], 
-    z3.Implies(z3.And(And_constraint, p3,p4,p5,p6, xor_property), 
-    z3.And(f_AND == ta_val, f_OR-f_AND == ta_mask)))
+#formula = z3.ForAll([X[0], X[1], X[2], X[3],ta_mask, ta_val], 
+#    z3.Implies(z3.And(And_constraint, p3,p4,p5,p6, xor_property), 
+#    z3.And(f_AND == ta_val, f_OR-f_AND == ta_mask)))
+formula = z3.Implies(z3.And(range_constraint,tnum_def_constraint , xor_property), 
+    z3.And(f_AND == ta_val, f_OR-f_AND == ta_mask))
+    
+#    z3.And(f_AND == ta_val, f_OR-f_AND == ta_mask)))
 z3.prove(formula)
 
 s = z3.Solver()

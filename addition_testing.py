2a0508a8310c3e326d91c8619ba6af2b18f952c0
mshachnai <m.shachnai@rutgers.edu>
2021-02-09 14:01:15 -0500

    added a check of set membership for variables in linux tnum_add

--- addition_testing.py
+++ addition_testing.py
@@ -17,6 +17,19 @@ def generate_tnums(n):
                 tnum_list.append((i,j))
     return tnum_list
 
+#function to count shared bits in order to derive minimal set that can be  
+#represented by tnum
+def max_prec_set(num, bits):
+    counter = 0
+
+    for i in range(bits):
+        if num & 2**i == 0:
+            counter += 1
+        #print(num, 2**i, counter)
+    
+    return 2**(bits-counter)
+
+
 
 #tnum range - takes min and max values of tnum interval range and returns a tnum
 #as defined by linux kernel
@@ -32,14 +45,34 @@ def tnum_range(min_a, max_a):
 
 #addition of two tnum numbers as defined in linux kernel
 def tnum_add(a, b, bits): 
+    #a_or = (a[0]+a[1])%2**bits
+    #b_or = (b[0]+b[1])%2**bits
+    #a_b_or_res = (a_or+b_or)%2**bits
     print("tnum a: ", a, "- tnum b: ", b)
     sm = a[1] + b[1] if a[1] + b[1] <=2**bits-1 else (a[1] + b[1])%2**bits
     sv = a[0] + b[0] if a[0] + b[0] <=2**bits-1 else (a[0] + b[0])%2**bits
     sigma = sm + sv if sm+sv <=2**bits-1 else (sm + sv)%2**bits
     chi = sigma ^ sv 
-    mu = chi | a[1] | b[1] 
+    mu = chi | a[1] | b[1]
+
     print("sm: ", sm, "\nsv: ", sv, "\nsigma: ", sigma, "\nchi: ", chi, "\nmu: ", mu,
         "\nresult: ", sv&~mu,",", mu)
+
+    if (sm & ~mu != sv & ~mu):
+        print("SM NOT IN set")
+    if (sv & ~mu != sv & ~mu):
+        print("SV NOT IN set")
+    if (sigma & ~mu != sv & ~mu):
+        print("SIGMA NOT IN set")
+    a_or = (a[0]+a[1])%2**bits
+    b_or = (b[0]+b[1])%2**bits
+    a_b_or_res = (a_or+b_or)%2**bits
+    if (a_b_or_res & ~mu != (sv & ~mu)):
+        print("OR NOT IN set", a_b_or_res & ~mu, mu, sv&~mu )
+    #if (((a[0]+a[1])+(b[0]+b[1]))&2**bits & ~mu != (sv & ~mu)):
+    #    print("OR NOT IN set", ((a[0]+a[1])+(b[0]+b[1]))%2**bits & ~mu, mu, sv&~mu )
+    #if ((b[0]+b[1]) & ~mu != sv & ~mu):
+    #    print("B_OR NOT IN set")
     return sv & ~mu, mu
 
 def new_tnum_add(a, b, bits):
@@ -49,8 +82,8 @@ def new_tnum_add(a, b, bits):
     for i in range(2**bits):
         if i & ~a[1] == a[0]: a_list.append(i)
         if i & ~b[1] == b[0]: b_list.append(i)
-    #addition of all elements in both lists nC2 - this is the actual range of the 
-    #new tnum
+    #addition of all elements in both lists (cartesian sum) - this is the concrete
+    #set of the new tnum
     print(a_list, b_list)
     for i in a_list:
         for j in b_list:
@@ -62,27 +95,42 @@ def new_tnum_add(a, b, bits):
     #values allowed for n-bits
     val = res[0]
     mask = res[0]
-    #this is a correct way of deriving val/mask from range full range
-    #for i in res:
-    #    val &= i
-    #    mask |= i
+    #this is a correct way of deriving val/mask from full range
+    for i in res:
+        val &= i
+        mask |= i
 
-    #val is the AND of all elements in range
-    #mask is OR of all elements in range minus val
-    ####correct code
-    #mask = mask-val
         
     #testing theory - do the same but only with max/min of values
     #####this approach doesn't work
     #val = (max(res) & min(res))
     #mask = (max(res) | min(res)) - val
-    
-    #comparison for tnum_add using tnum_range - cartesian sum of two tnum ranges
-    #and take tnum_range
-    
-    val, mask = tnum_range(min(res), max(res))
 
+    #comparison for tnum_add using tnum_range - cartesian sum of two tnum ranges
+    #and take tnum_range to test precision vs tnum_add
+    #val, mask = tnum_range(min(res), max(res))
+
+    
+    #check if set is maximally precise by taking val and mask(this is before mask is
+    #derived fully, still need to do OR-AND) and performing xor
+    #if bit at ith place is 0, bit is shared 
+    shared_bits = val ^ mask
+    optimal_set_size = max_prec_set(shared_bits, bits)
+    print("optimal set size:", optimal_set_size)
+
+    
+    #val is the AND of all elements in range
+    #mask is OR of all elements in range minus val
+    ####correct code
+    mask = mask-val
     print("result: ", val, ",", mask)
+    #range of the outcome tnum c for observation
+    prec_list = []
+    for i in range(2**bits):
+        if i & ~mask == val: prec_list.append(i)
+    print(prec_list)
+    if len(prec_list) != optimal_set_size : print("NOT MAXIMALLY PRECISE")
+
     return val, mask
 
 
@@ -98,7 +146,7 @@ def valid_tnum(val, mask, bits):
 
 #################################### MAIN FUNCTION
 def main():
-    for i in range(5):
+    for i in range(6):
         #define size of tnum and number of valid values
         n = i
         valid_set = 3**n
@@ -133,7 +181,7 @@ def main():
                     count += 1
         else:
             #since tnum is commutative, we can make list of all combinations rather than 
-            #checking all 3^n * 3^n options
+            #checking all 3^n + 3^n options
             comb = combinations(tnum_values, 2)
             #combine all possible valid values 3**n choose 2
             for i in list(comb):  
